2fe962e884b0dfd24553b50e2a9ba5173b278c01
James Garcia <james.jgarciap@gmail.com>
2024-01-13 14:53:28 +0100

    initial commit

---
+++ hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func Hello() string {
+	return "Hello, World!"
+}
+
+// main is our application entry point
+// private methods names start with lowercase, and so public ones start with uppercase
+func main() {
+	fmt.Println("Holis")
+}
